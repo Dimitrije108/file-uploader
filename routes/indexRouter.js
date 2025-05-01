9c840ce8d4499f95ea733e8811ea3441bce2cf82
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-05-01 21:09:50 +0200

    feat: implement multer and the functionality to upload files

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,11 +1,35 @@
 const { Router } = require('express');
 const indexRouter = Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const multer = require('multer');
+// this is where the files will be stored
+const upload = multer({ dest: 'public/uploads/' });
 
-indexRouter.get('/', (req, res) => {
-	// insert controller
+indexRouter.get('/', async (req, res) => {
+	
+	const folders = await prisma.folder.findMany({
+		where: {
+			userId: req.user.id,
+		}
+	});
+
 	res.render('index', {
+		user: req.user,
+		folders
+	});
+});
+
+indexRouter.get('/upload', (req, res) => {
+	res.render('upload', {
 		user: req.user
 	});
+});
+
+indexRouter.post('/upload', upload.array('files', 12), (req, res) => {
+	console.log(req.files);
+	res.redirect('/upload');
+	// we will think about folders later and how to upload into them
 });
 
 module.exports = indexRouter;
